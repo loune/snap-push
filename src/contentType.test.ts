b9069b62b7b22032112a24e4d9b2ae71599dfdc0
Loune <loune-git@siphon9.net>
2023-10-25 20:44:04 +1100

    feat: Refactor content type detection

--- src/contentType.test.ts
+++ src/contentType.test.ts
@@ -1,10 +1,9 @@
 import fs from 'fs';
-import Mime from 'mime/Mime';
 import getFileMimeType from './contentType.js';
 
 jest.setTimeout(10000);
 
-test('test content type html', async () => {
+test('content type html', async () => {
   const filename = `test-html-${Date.now()}`;
   const ws = fs.createWriteStream(filename, { encoding: 'utf8', flags: 'as' });
   ws.write('<!DOCTYPE html>\n<html></html>');
@@ -18,7 +17,7 @@ test('test content type html', async () => {
   fs.unlinkSync(filename);
 });
 
-test('test content type not html', async () => {
+test('content type not html', async () => {
   const filename = `test-nothtml-${Date.now()}`;
   const ws = fs.createWriteStream(filename, { encoding: 'utf8', flags: 'as' });
   ws.write('<!DOCTYsdfsdfsPE htmdsfdsfdsl>dfdsn<htmsadsfl></html>');
@@ -26,13 +25,13 @@ test('test content type not html', async () => {
 
   const type = await getFileMimeType(filename);
 
-  expect(type).toEqual(null);
+  expect(type).toEqual(undefined);
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
   fs.unlinkSync(filename);
 });
 
-test('test content type txt', async () => {
+test('content type txt', async () => {
   const filename = `test-text-${Date.now()}.txt`;
   const ws = fs.createWriteStream(filename, { encoding: 'utf8', flags: 'as' });
   ws.write('<!DOCTYPE html>\n<html></html>');
@@ -46,26 +45,23 @@ test('test content type txt', async () => {
   fs.unlinkSync(filename);
 });
 
-test('test content type override (Mime class)', async () => {
-  const filename = `test-custom-${Date.now()}.pub`;
-  // eslint-disable-next-line global-require,@typescript-eslint/no-var-requires
-  const customMime = new Mime(require('mime/types/standard'), require('mime/types/other'));
-  customMime.define({ 'text/plain': ['pub'] }, true);
-  const type = await getFileMimeType(filename, customMime);
-
-  expect(type).toEqual('text/plain');
-});
-
-test('test content type override', async () => {
+test('content type override', async () => {
   const filename = `test-custom-${Date.now()}.pub`;
   const type = await getFileMimeType(filename, { 'text/plain': ['pub'] });
 
   expect(type).toEqual('text/plain');
 });
 
-test('test content type override with fallback', async () => {
+test('content type override with fallback', async () => {
   const filename = `test-custom-${Date.now()}.svg`;
   const type = await getFileMimeType(filename, { 'text/plain': ['pub'] });
 
   expect(type).toEqual('image/svg+xml');
+});
+
+test('content type xml', async () => {
+  const filename = `test-custom-${Date.now()}.xml`;
+  const type = await getFileMimeType(filename);
+
+  expect(type).toEqual('application/xml');
 });
--- src/contentType.ts
+++ src/contentType.ts
@@ -1,6 +1,101 @@
 import fs from 'fs';
-import mime from 'mime';
-import Mime from 'mime/Mime.js';
+
+const standardContentTypeExtMap = new Map<string, string[]>([
+  ['audio/aac', ['aac']],
+  ['application/x-abiword', ['abw']],
+  ['application/x-freearc', ['arc']],
+  ['image/avif', ['avif']],
+  ['video/x-msvideo', ['avi']],
+  ['application/vnd.amazon.ebook', ['azw']],
+  ['application/octet-stream', ['bin']],
+  ['image/bmp', ['bmp']],
+  ['application/x-bzip', ['bz']],
+  ['application/x-bzip2', ['bz2']],
+  ['application/x-cdf', ['cda']],
+  ['application/x-csh', ['csh']],
+  ['text/css', ['css']],
+  ['text/csv', ['csv']],
+  ['application/msword', ['doc']],
+  ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', ['docx']],
+  ['application/vnd.ms-fontobject', ['eot']],
+  ['application/epub+zip', ['epub']],
+  ['application/gzip', ['gz']],
+  ['image/gif', ['gif']],
+  ['text/html', ['htm', 'html']],
+  ['image/vnd.microsoft.icon', ['ico']],
+  ['text/calendar', ['ics']],
+  ['application/java-archive', ['jar']],
+  ['image/jpeg', ['jpeg', 'jpg']],
+  ['text/javascript', ['js']],
+  ['application/json', ['json']],
+  ['application/ld+json', ['jsonld']],
+  ['audio/midi', ['mid, .midi']],
+  ['audio/x-midi', ['mid, .midi']],
+  ['text/javascript', ['mjs']],
+  ['audio/mpeg', ['mp3']],
+  ['video/mp4', ['mp4']],
+  ['video/mpeg', ['mpeg']],
+  ['application/vnd.apple.installer+xml', ['mpkg']],
+  ['application/vnd.oasis.opendocument.presentation', ['odp']],
+  ['application/vnd.oasis.opendocument.spreadsheet', ['ods']],
+  ['application/vnd.oasis.opendocument.text', ['odt']],
+  ['audio/ogg', ['oga']],
+  ['video/ogg', ['ogv']],
+  ['application/ogg', ['ogx']],
+  ['audio/opus', ['opus']],
+  ['font/otf', ['otf']],
+  ['image/png', ['png']],
+  ['application/pdf', ['pdf']],
+  ['application/x-httpd-php', ['php']],
+  ['application/vnd.ms-powerpoint', ['ppt']],
+  ['application/vnd.openxmlformats-officedocument.presentationml.presentation', ['pptx']],
+  ['application/vnd.rar', ['rar']],
+  ['application/rtf', ['rtf']],
+  ['application/x-sh', ['sh']],
+  ['image/svg+xml', ['svg']],
+  ['application/x-tar', ['tar']],
+  ['image/tiff', ['tif, .tiff']],
+  ['video/mp2t', ['ts']],
+  ['font/ttf', ['ttf']],
+  ['text/plain', ['txt']],
+  ['application/vnd.visio', ['vsd']],
+  ['audio/wav', ['wav']],
+  ['audio/webm', ['weba']],
+  ['video/webm', ['webm']],
+  ['image/webp', ['webp']],
+  ['font/woff', ['woff']],
+  ['font/woff2', ['woff2']],
+  ['application/xhtml+xml', ['xhtml']],
+  ['application/vnd.ms-excel', ['xls']],
+  ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', ['xlsx']],
+  ['application/xml', ['xml']],
+  ['text/xml', ['xml']],
+  ['application/vnd.mozilla.xul+xml', ['xul']],
+  ['application/zip', ['zip']],
+  ['video/3gpp', ['3gp']],
+  ['video/3gpp2', ['3g2']],
+  ['application/x-7z-compressed', ['7z']],
+]);
+
+function reverseMap(contentTypeExtMap: Map<string, string[]> | Record<string, string[]>): Map<string, string[]> {
+  const newMap = new Map<string, string[]>();
+  const entries = contentTypeExtMap instanceof Map ? contentTypeExtMap.entries() : Object.entries(contentTypeExtMap);
+  for (const entry of entries) {
+    for (const key of entry[1]) {
+      let values = newMap.get(key);
+      if (!values) {
+        values = [];
+        newMap.set(key, values);
+      }
+
+      values.push(entry[0]);
+    }
+  }
+
+  return newMap;
+}
+
+const standardExtContentTypeMap = reverseMap(standardContentTypeExtMap);
 
 async function readChars(filename: string, numOfChars: number): Promise<string> {
   const buf: (Buffer | string)[] = [];
@@ -24,21 +119,32 @@ async function readChars(filename: string, numOfChars: number): Promise<string> 
   });
 }
 
-export default async function getFileMimeType(filename: string, customMime: any = null): Promise<string> {
-  if (customMime && (!customMime.constructor || customMime.constructor.name !== 'Mime')) {
-    const types = customMime;
-    // eslint-disable-next-line global-require,@typescript-eslint/no-var-requires
-    customMime = new Mime(require('mime/types/standard'), require('mime/types/other'));
-    customMime.define(types, true);
+export default async function getFileMimeType(
+  filename: string,
+  customMimeTypes?: Record<string, string[]> | undefined
+): Promise<string | undefined> {
+  const [, extension] = filename.split('.');
+
+  let type: string | undefined;
+  if (extension) {
+    if (customMimeTypes) {
+      const customExtContentTypeMap = reverseMap(customMimeTypes);
+
+      type = customExtContentTypeMap.get(extension)?.[0];
+      if (type !== undefined) {
+        return type;
+      }
+    }
+
+    type = standardExtContentTypeMap.get(extension)?.[0];
   }
-  const myMime = customMime || mime;
-  let type = myMime.getType(filename);
-  if (type === null) {
+
+  if (type === undefined) {
     try {
       const chars = await readChars(filename, 200);
       const charsLower = chars.toLowerCase();
       if (charsLower.indexOf('<html>') !== -1 || charsLower.indexOf('<!doctype html>') !== -1) {
-        type = myMime.getType('.html');
+        type = standardExtContentTypeMap.get('html')?.[0];
       }
       // eslint-disable-next-line no-empty
     } catch (err) {}
